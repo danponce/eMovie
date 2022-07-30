4f9073fe431fe240c2295ac5a2862effd8f104df
Dan Ponce <dan.ponce@carsales.com.au>
2022-07-30 07:48:25 -0400

    Remove unused code

--- app/src/main/java/au/com/carsales/emovie/ui/detail/MovieDetailViewModel.kt
+++ app/src/main/java/au/com/carsales/emovie/ui/detail/MovieDetailViewModel.kt
@@ -2,13 +2,9 @@ package au.com.carsales.emovie.ui.detail
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
-import au.com.carsales.emovie.ui.detail.episode.EpisodesSeason
 import au.com.carsales.emovie.ui.model.UIMovieItem
-import au.com.carsales.emovie.utils.base.State
 import au.com.carsales.emovie.utils.base.viewmodel.BaseBindingViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -26,50 +22,6 @@ class MovieDetailViewModel @Inject constructor(
     private val _isFavoriteLiveData = MutableLiveData<Boolean>()
     val isFavoriteLiveData : LiveData<Boolean> = _isFavoriteLiveData
 
-//    private val _episodesLiveData = MutableLiveData<State<List<EpisodeViewData>>>()
-//    val episodesLiveData : LiveData<State<List<EpisodeViewData>>> = _episodesLiveData
-//
-//    val episodeClickedLiveData = MutableLiveData<EpisodeViewData>()
-//
-//    var episodeList : List<EpisodeViewData>?= null
-//
-//    fun setClickedEpisode(episode : EpisodeViewData) {
-//        episodeClickedLiveData.postValue(episode)
-//    }
-
-//    private suspend fun getEpisodesRequest () = withContext(Dispatchers.IO) {
-//        getEpisodesUseCase.getEpisodes(showId ?: 0)
-//    }
-
-    fun getEpisodes() {
-        viewModelScope.launch {
-            setLoadingStatus()
-
-//            processAsyncJob(
-//                block = { getEpisodesRequest() },
-//                result = {
-//                    if(it.isNullOrEmpty()) {
-//                        _episodesLiveData.postValue(State.error())
-//
-//                        setErrorStatus()
-//                    } else {
-////                        episodeList = it.mapNotNull { item -> episodesMapper.executeMapping(item) }
-////
-////                        _episodesLiveData.postValue(State.success(episodeList ?: listOf()))
-//
-//                        setSuccessStatus()
-//                    }
-//
-//                },
-//                onError = {
-//                    _episodesLiveData.postValue(State.error())
-//
-//                    setErrorStatus()
-//                }
-//            )
-        }
-    }
-
     fun setMovie(data : UIMovieItem) {
         movieItemLiveData.postValue(data)
 //        showId = data.id
@@ -77,29 +29,6 @@ class MovieDetailViewModel @Inject constructor(
     }
 
     fun getLastShow() = movieItem
-
-//    fun getEpisodesSeasons() : List<EpisodesSeason> {
-//
-//        val episodesMap = hashMapOf<Int, ArrayList<EpisodeViewData>>()
-//
-//        episodeList?.forEach { episode ->
-//            if(episodesMap.contains(episode.season)) {
-//                episodesMap[episode.season]?.add(episode)
-//            } else {
-//                episodesMap[episode.season ?: 0] = arrayListOf(episode)
-//            }
-//        }
-//
-//        val seasonList = arrayListOf<EpisodesSeason>()
-//
-//        episodesMap.entries.forEach { entry ->
-//            seasonList.add(EpisodesSeason(entry.key, entry.value))
-//        }
-//
-//        return seasonList
-//    }
-//
-//    fun getExistentEpisodesData(): List<EpisodeViewData>? = episodeList
 
     fun isShowFavorite() {
 //        viewModelScope.launch(Dispatchers.IO) {
