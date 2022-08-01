06c7f5a423b386771ff27a2b307f6f11ca87a7ed
Dan Ponce <dan.ponce@carsales.com.au>
2022-08-01 12:11:53 -0400

    Add base data binding view component

---
+++ app/src/main/java/au/com/carsales/emovie/utils/base/viewcomponent/BaseDataBindingViewComponent.kt
@@ -0,0 +1,35 @@
+package au.com.carsales.emovie.utils.base.viewcomponent
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.RelativeLayout
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+/**
+ * Created by Dan on 01, agosto, 2022
+ * Copyright (c) 2022 Carsales. All rights reserved.
+ */
+abstract class BaseDataBindingViewComponent <T : ViewDataBinding> : RelativeLayout {
+
+    abstract fun layoutId() : Int
+    lateinit var binding: T
+
+    private fun initBinding() {
+        binding = DataBindingUtil.inflate(LayoutInflater.from(context), layoutId(), this, false)
+        addView(binding.root)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initBinding()
+    }
+
+    constructor(context: Context) : super(context) {
+        initBinding()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initBinding()
+    }
+}
