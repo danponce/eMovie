d5aa2747e5beb0e1e51887c23f4a9ef82eed7739
Dan Ponce <dan.ponce@carsales.com.au>
2022-08-05 10:07:11 -0400

    Add splash functionality

--- app/src/main/java/au/com/carsales/emovie/MoviesActivity.kt
+++ app/src/main/java/au/com/carsales/emovie/MoviesActivity.kt
@@ -1,5 +1,7 @@
 package au.com.carsales.emovie
 
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.viewbinding.ViewBinding
 import au.com.carsales.emovie.databinding.ActivityMainBinding
 import au.com.carsales.emovie.utils.base.BaseNavActivity
@@ -9,8 +11,34 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MoviesActivity : BaseNavActivity() {
 
+    companion object {
+        const val splashFadeDurationMillis = 500
+    }
+
     override fun layoutViewBinding(): ViewBinding = ActivityMainBinding.inflate(layoutInflater)
     override fun navHostFragmentId(): Int = R.id.nav_host_fragment
     override fun bottomNavigationView(): BottomNavigationView = (binding as ActivityMainBinding).bottomNavView
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashWasDisplayed = savedInstanceState != null
+
+        if (!splashWasDisplayed) {
+            val splashScreen = installSplashScreen()
+//            splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+//                splashScreenViewProvider.iconView
+//                    .animate()
+//                    .setDuration(splashFadeDurationMillis.toLong())
+//                    .alpha(0f)
+//                    .withEndAction {
+//                        splashScreenViewProvider.remove()
+//                        super.onCreate(savedInstanceState)
+//                    }.start()
+//            }
+            super.onCreate(savedInstanceState)
+        } else {
+            super.onCreate(savedInstanceState)
+        }
+
+    }
 }
